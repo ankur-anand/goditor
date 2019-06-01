e793d1e07cc19e53155049a3b8a87058ad7c5b81
Ankur Anand <ankur.anand12@gmail.com>
2019-06-01 11:38:58 +0530

    screen state initialized

--- src/editor/editor.go
+++ src/editor/editor.go
@@ -7,7 +7,6 @@ import (
 	"github.com/ankur-anand/goditor/src/keyboard"
 	"github.com/ankur-anand/goditor/src/screen"
 	"github.com/ankur-anand/goditor/src/terminal"
-	"golang.org/x/sys/unix"
 )
 
 const (
@@ -16,37 +15,21 @@ const (
 	goditorVersion = 0.1
 )
 
-// state is to keep track of the cursor’s x and y position
-// and Winsize
-type state struct {
-	curX    uint16
-	curY    uint16
-	winsize *unix.Winsize
-}
-
-// initialize a new goditor state.
-func newState() state {
-	// we initialize the both cursor at position
-	// at X:1 and Y:1 where X is row and Y is Column.
-	s := state{curX: 1, curY: 1, winsize: terminal.WinSize()}
-	return s
-}
-
 type editor struct {
 	term *terminal.Term
 }
 
 // StartGoditor starts a new Editor
 func StartGoditor() {
-	//s := newState()
 	e := editor{
 		term: terminal.CurrentTerm(),
 	}
+	scrn := screen.NewState()
 	// enable raw mode for the terminal
 	e.term.EnableRawMode()
 	for {
 		readValue := e.keyboardRead()
-		screen.ProcessKey(readValue)
+		scrn.ProcessKey(readValue)
 	}
 }
 
--- src/screen/screen.go
+++ src/screen/screen.go
@@ -6,9 +6,27 @@ import (
 	"os"
 
 	"github.com/ankur-anand/goditor/src/keyboard"
+	"github.com/ankur-anand/goditor/src/terminal"
+	"golang.org/x/sys/unix"
 )
 
 // get the current window size of terminal.
+
+// State is to keep track of the cursor’s x and y position
+// and Winsize
+type State struct {
+	curX    uint16
+	curY    uint16
+	winsize *unix.Winsize
+}
+
+// NewState initialize a new goditor state which manages the screen of editor.
+func NewState() *State {
+	// we initialize the both cursor at position
+	// at X:1 and Y:1 where X is row and Y is Column.
+	s := &State{curX: 1, curY: 1, winsize: terminal.WinSize()}
+	return s
+}
 
 // WriteToStdOut writes n bytes to the os.Stdout file
 func WriteToStdOut(value string) error {
@@ -33,7 +51,7 @@ func ClearScreen() {
 }
 
 // ProcessKey waits for a keypress recieved, and then handles it
-func ProcessKey(key keyboard.Key) {
+func (s *State) ProcessKey(key keyboard.Key) {
 
 	switch key {
 
